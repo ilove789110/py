fe442b8607424e5dbca6e299e6a12161b509959c
kangliu <kangliu@Ctrip.com>
2016-02-16 16:50:14 +0800

    update

--- circumference.py
+++ circumference.py
@@ -11,4 +11,8 @@ area = math.pi * (radiusInt ** 2)
 print "The circumference is: ", circumference, \
     "The area is :", area
 
+# id
 print "The id of area object: ", id(area)
+
+# type
+print "The type of area object: ", type(area)
---
+++ id-type.py
@@ -0,0 +1,14 @@
+aInt = 7
+
+print "aInt id:", id(aInt), \
+    "aInt type:", type(aInt)
+
+bFloat = 2.5
+
+print "bFloat id:", id(bFloat), \
+    "bFloat type:", type(bFloat)
+
+aInt = bFloat
+
+print "aInt id:", id(aInt), \
+    "aInt type:", type(aInt)
