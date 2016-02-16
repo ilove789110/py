0fa57d1669e16d339b5de999a97b3b86fbca37ca
kangliu <kangliu@Ctrip.com>
2016-02-16 18:56:57 +0800

    update

---
+++ type/constructor.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+print int("1")
+
+print int(1.7)
+
+print int(True)
+
+# print int('1.1')  # can't go from a string, to float, to an int. too far
+
+print float('3.33')
+
+print float(42)
+
+print str(9.9)
+
+myVar = '2'
+
+print myVar
+
+print int(myVar)
+
+print myVar
+
+myVar = int(myVar)
+
+print myVar
