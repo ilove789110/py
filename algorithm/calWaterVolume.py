8d56edd95ecbf11cdde019898e0724110003164b
kangliu <kangliu@Ctrip.com>
2016-02-17 13:49:01 +0800

    update

---
+++ algorithm/calWaterVolume.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+waterVolume = 1.0 / 12 * 43560
+
+gallons = waterVolume * 7.48051945
+
+print "1 in. rain on 1 acre is", gallons, "gallons"
---
+++ algorithm/draw5star.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import turtle
+
+turtle.forward(100)  # draw line
+
+turtle.right(144)  # turn angle
+
+turtle.forward(100)
+
+turtle.right(144)
+
+turtle.forward(100)
+
+turtle.right(144)
+
+turtle.forward(100)
+
+turtle.right(144)
+
+turtle.forward(100)
