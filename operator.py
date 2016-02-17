3d03fdfb6fe754070dfa82fde0fcc9a87158886b
kangliu <kangliu@Ctrip.com>
2016-02-17 13:30:38 +0800

    update

---
+++ operator.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+# int运算符
+
+print 5 / 3  # 求商
+
+print 5 % 3  # 求余
+
+# float运算符
+
+print 5.0 / 3.0
+
+intVar = 42
+
+floatVar = 42.0
+
+print intVar / 5
+
+print floatVar / 5
+
+print intVar / 5.0  # intVar提升转成float
+
+print floatVar / 5.0
+
+print 2.0 / 3
+
+print 2 + 3 * 5 ** 2  # exponents before multiplication before addition
+
+# operator shortcut 不能用于定义变量
+
+myVar = 0
+
+myVar += 2  # myVar=myVar+2
+
+myVar -= 1  # myVar=myVar-1
+
+myVar /= 1  # myVar=myVar/1
+
+myVar *= 1  # myVar=myVar*1
+
+print myVar
+
+intA = 10
+intB = 3
+intC = 0
+
+print intA+intB*intC
