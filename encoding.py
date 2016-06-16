696675fc6630877e39e7818f9c3c8ffda42d2c20
kangliu <kangliu@Ctrip.com>
2016-06-16 18:16:59 +0800

    update kNN sample

---
+++ encoding.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+print ord('A')
+
+print chr(65)
+
+print unichr(65)
+
+print u'中文'
+
+u_ = u'中'
+
+print u_
+print u_.encode('utf-8')
+
+print '\xe4\xb8\xad'.decode('utf-8')
--- innerDataStructures/tuple.py
+++ innerDataStructures/tuple.py
@@ -4,14 +4,13 @@ t = ('Adam', 'Lisa', 'Bart')
 
 tb = ('Apple',)
 
-print t[1]
-
+print "get element", t[1]
 
 print t[0:1]  # 半闭半开区间  切片
 
 print t[0:2]
 
-print t[0:2:1] #设置了步长的切片
+print t[0:2:1]  # 设置了步长的切片
 
 print t[0:2:2]
 
